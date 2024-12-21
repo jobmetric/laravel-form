bcec76d0f23c68f011c9cbc69e7d4c081ed31a8d
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-21 22:43:42 +0330

    added form service provider

---
+++ src/FormServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace JobMetric\Form;
+
+use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class FormServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws ViewFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-form')
+            ->hasTranslation()
+            ->hasView();
+    }
+}
