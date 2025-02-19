6c58411889734466c87d471fc261c33f28db4c0a
hassan7303 <hassanali7303@gmail.com>
2025-02-19 21:38:34 +0330

    added fa languege

---
+++ lang/fa/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Custom Field Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Custom Field for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+    ],
+
+    'exceptions' => [
+        'option_empty_label' => 'Option label cannot be empty.',
+    ],
+
+    'messages' => [
+    ],
+
+];
